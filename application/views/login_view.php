53cab5ab8c96fa492ff2a16dd35a95d7611c6449
Ovybe1902 <aroandrianimanana@gmail.com>
2023-07-10 08:06:02 +0200

    Ajax view

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -6,6 +6,69 @@
     <title>Document</title>
 </head>
 <!-- check login via httprequest ajax -->
+<script>
+    document.addEventListener(
+        'DOMContentLoaded', function(){
+            var loginForm = document.getElementById('loginForm');
+            var signupForm = document.getElementById('signupForm');
+            var errorContainer = document.getElementById('errorContainer');
+            var errorSignup = document.getElementById('errorSignup');
+
+            // login request
+            loginForm.addEventListener('submit', function(e){
+                e.preventDefault();
+
+                var formData = new FormData(loginForm);
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", '<?php echo base_url("Profile_Controller/CheckLogin") ; ?>' , true);
+
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState === 4){
+                        console.log("HI IM HERE");
+
+                        if(xhr.status === 200){
+                            if(xhr.responseText === 'success'){
+                                window.location.href = '<?php echo base_url("Home_Controller") ; ?>';
+                            }else{
+                                errorContainer.textContent = 'Identifiants Invalides';
+                            }
+                        }else{
+                            errorContainer.textContent = 'Erreur lors de la requete';
+                        }
+                    }
+                    console.log(xhr.readyState);
+                }
+                xhr.send(formData);
+            });
+
+            // signup request
+            signupForm.addEventListener('submit', function(e){
+                e.preventDefault();
+
+                var formData = new FormData(signupForm);
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", '<?php echo base_url("Profile_Controller/CheckSignUp") ; ?>' , true);
+
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState === 4){
+                        console.log("HI IM HERE NOW");
+
+                        if(xhr.status === 200){
+                            if(xhr.responseText === 'success'){
+                                window.location.href = '<?php echo base_url("Home_Controller") ; ?>';
+                            }else{
+                                errorSignup.textContent = 'Formulaires Invalides';
+                            }
+                        }else{
+                            errorSignup.textContent = 'Erreur lors de la requete';
+                        }
+                    }
+                    console.log(xhr.readyState);
+                }
+                xhr.send(formData);
+            });
+        });
+</script>
 
 <body>
     <div class="login-session">
