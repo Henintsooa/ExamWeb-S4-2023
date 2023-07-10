942dbf3b4d1c9491e97a6f1fcfea2049de5fa7e1
Ovybe1902 <aroandrianimanana@gmail.com>
2023-07-10 19:25:08 +0200

    Front Office Control

--- application/views/frontoffice_view.php
+++ application/views/frontoffice_view.php
@@ -9,7 +9,9 @@
     document.addEventListener(
         'DOMContentLoaded', function(){
             var objectifForm = document.getElementById('objectifForm');
+            var userDataForm = document.getElementById('userDataForm');
             var errorContainerobjectif = document.getElementById('errorContainerObjectif');
+            var errorContainerUserdata = document.getElementById('errorContainerUserdata');
 
             // objectif request
             objectifForm.addEventListener('submit', function(e){
@@ -37,20 +39,54 @@
                 }
                 xhr.send(formData);
             });
+
+            userDataForm.addEventListener('submit', function(e){
+                e.preventDefault();
+
+                var formData = new FormData(userDataForm);
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", '<?php echo base_url("UserData_Controller/insertuserdata") ; ?>' , true);
+
+                xhr.onreadystatechange = function() {
+                    if(xhr.readyState === 4){
+                        console.log("HI IM HERE");
+
+                        if(xhr.status === 200){
+                            if(xhr.responseText === 'success'){
+                                window.location.href = '<?php echo base_url("Home_Controller/loadobjectifinterface") ; ?>';
+                            }else{
+                                errorContainerobjectif.textContent = 'Donnes Invalides';
+                            }
+                        }else{
+                            errorContainerobjectif.textCont1ent = 'Erreur lors de la requete';
+                        }
+                    }
+                    console.log(xhr.readyState);
+                }
+                xhr.send(formData);
+            });
         });
 
 </script>
 <body>
 
+    <h1>HELLO YOU <?php echo $userdata['username'] ;?> </h1>
+    <p>
+        <?php
+            var_dump($userdata);
+        ?>
+    </p>
+
     <!-- form userdata -->    
     <div class="userDataForm">
         <h1>Insertion userdata</h1>
-        <form method="POST" id="objectifForm" >
+        <form method="POST" id="userdataForm" >
 
+            <label for="sexe">sexe :</label>
             <select name="sexe" id="sexe">
                 <option value="0">homme</option>
                 <option value="1">femme</option>
-            </select>
+            </select><br><br>
 
             <label for="taille">taille :</label>
             <input type="number" step="1" name="taille" id="taille" required min=0><br><br>
@@ -60,7 +96,7 @@
 
             <input type="submit" value="Suggerer regimes">
         </form> 
-        <p id="errorContainerObjectif"></p>
+        <p id="errorContainerUserdata"></p>
     </div>
     
     <!-- form regime -->
