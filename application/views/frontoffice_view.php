fc89996d6c49b6e31828c7aba3f6fa2df90f5b9a
kathyrazaf01 <120714714+kathyrazaf01@users.noreply.github.com>
2023-07-11 13:01:16 +0300

    frontOffice design

--- application/views/frontoffice_view.php
+++ application/views/frontoffice_view.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url("assets/fonts/icomoon/style.css")?>">
+    <link rel="stylesheet" href="<?= base_url("assets/css/bootstrap.min.css")?>">
+    <link rel="stylesheet" href="<?= base_url("assets/css/style.css")?>">
+    <title>Accueil</title>
 </head>
 <script>
     document.addEventListener(
@@ -112,62 +116,104 @@
 </script>
 <body>
 
-    <h1>HELLO YOU <?php echo $userdata['username'] ;?> </h1>
-    <p>
-        <?php
-            var_dump($userdata);
-        ?>
-    </p>
+        <div class="content">
+            <div class="container">
+                <div class="row align-items-stretch no-gutters contact-wrap">
+                    <div class="col-md-12">
+                        <div class="form h-100">
+                            <h3>Bienvenu(e), <?php echo $userdata['username'] ;?> </h3>
+                            <!-- <p>
+                                <!-- <?php
+                                    var_dump($userdata);
+                                ?>
+                            </p> -->
 
-    <!-- form userdata -->    
-    <div class="userDataForm">
-        <h1>Insertion userdata</h1>
-        <form method="POST" id="userdataForm" >
+                            <!-- form userdata -->    
+                            <div class="userDataForm">
+                                <h4>Vos informations</h4>
+                                <form class="mb-5" method="POST" id="userdataForm" name="contactForm">
+                                <div class="row">
+                                    <div class="col-md-6 form-group mb-3">
+                                        <label for="sexe" class="col-form-label">sexe </label>
+                                        <select class="form-control" name="sexe" id="sexe">
+                                            <option value="0">homme</option>
+                                            <option value="1">femme</option>
+                                        </select>
+                                    </div>
+                                </div>
 
-            <label for="sexe">sexe :</label>
-            <select name="sexe" id="sexe">
-                <option value="0">homme</option>
-                <option value="1">femme</option>
-            </select><br><br>
+                                <div class="row">
+                                    <div class="col-md-6 form-group mb-3">
+                                        <label class="col-form-label" for="taille">taille </label>
+                                        <input class="form-control" type="number" step="1" name="taille" id="taille" required min=0>
+                                    </div>
+                                    <div class="col-md-6 form-group mb-3">
+                                        <label  class="col-form-label" for="poids">poids :</label>
+                                        <input class="form-control" type="number" step="1" name="poids" id="poids" required min=0>
+                                    </div>
+                                </div>
 
-            <label for="taille">taille :</label>
-            <input type="number" step="1" name="taille" id="taille" required min=0><br><br>
+                                <div class="row">
+                                    <div class="col-md-6 form-group mb-3">
+                                        <label for="date" class="col-form-label">Sélectionnez une date </label>
+                                        <input class="form-control" type="date" id="date" name="date" required>
+                                   
+                                    </div>
+                                </div>
 
-            <label for="poids">poids :</label>
-            <input type="number" step="1" name="poids" id="poids" required min=0><br><br>
+                                <div class="row">
+                                    <div class="col-md-12 form-group">
+                                        <input type="submit" value="Valider" class="btn btn-primary rounded-0 py-2 px-4">
+                                    </div>
+                                </div>
+                                </form> 
+                                <p id="errorContainerUserdata"></p>
+                            </div>
+                            <!-- end form userdata -->  
+                            
+                            <!-- form regime -->
+                            <div class="objectif_form">
+                                <h4>Votre Objectif</h4>
+                                <form class="mb-5" method="POST" id="objectifForm" name="contactForm" >
+                                    <div class="row">
+                                        <div class="col-md-6 form-group mb-3">
+                                            <label class="col-form-label" for="objectif">objectif :</label>
+                                            <input class="form-control" type="number" step="1" name="objectif" id="objectif" required>
+                                        </div>
+                                    </div>
+                                
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                        <input type="submit" value="Suggerer regimes" class="btn btn-primary rounded-0 py-2 px-4>
+                                        </div>
+                                    </div>
+                                   
+                                </form>
+                                <p id="errorContainerObjectif"></p>
+                            </div>
+                             <!-- end form regime --> 
 
-            <label for="date">Sélectionnez une date :</label>
-            <input type="date" id="date" name="date" required>
-            <br><br>
+                            <!-- list suggestion -->
+                            <div id="listsuggestion">
 
-            <input type="submit" value="inserer data">
-        </form> 
-        <p id="errorContainerUserdata"></p>
-    </div>
-    
-    <!-- form regime -->
-    <div class="objectif_form">
-        <h1>INSERTION Objectifs</h1>
-        <form method="POST" id="objectifForm" >
+                            </div>
 
-            <label for="objectif">objectif :</label>
-            <input type="number" step="1" name="objectif" id="objectif" required><br><br>
+                            <div class="listcode">
+                                <?php foreach($codes as $code ){ ?>
+                                    <p><?php echo $code['code'] ;?> - <?php echo $code['montant'] ;?> Ar </p>
+                                    <a href="<?php echo base_url("Code_Controller/getcode?id=".$code['idCode']) ; ?>"> get code </a>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-            <input type="submit" value="Suggerer regimes">
-        </form>
-        <p id="errorContainerObjectif"></p>
-    </div>
-
-    <!-- list suggestion -->
-    <div id="listsuggestion">
-
-    </div>
-
-    <div class="listcode">
-        <?php foreach($codes as $code ){ ?>
-            <p><?php echo $code['code'] ;?> - <?php echo $code['montant'] ;?> Ar </p>
-            <a href="<?php echo base_url("Code_Controller/getcode?id=".$code['idCode']) ; ?>"> get code </a>
-        <?php } ?>
-    </div>
+    <script src="<?= base_url("assets/js/jquery-3.3.1.min.js")?>"></script>
+    <script src="<?= base_url("assets/js/popper.min.js")?>"></script>
+    <script src="<?= base_url("assets/js/bootstrap.min.js")?>"></script>
+    <script src=" <?= base_url("assets/js/jquery.validate.min.js")?>"></script>
+    <script src="<?= base_url("assets/js/jquery.validate.min.js")?>"></script>
 </body>
 </html>
